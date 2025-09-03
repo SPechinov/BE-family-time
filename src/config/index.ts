d1e2ca4ed513767b2ca1bd2c2643d1bfb0586495
sergeipechinov <spechinov@gmail.com>
2025-09-03 17:18:30 +0400

    Init

---
+++ src/config/index.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
+import { z } from 'zod';
+
+const ConfigSchema = z.object({
+  server: z.object({
+    port: z.number(),
+  }),
+  postgres: z.object({
+    uri: z.string(),
+  }),
+  redis: z.object({
+    uri: z.string(),
+  }),
+});
+
+const loadFile = (uri: string) => {
+  const envFile = fs.readFileSync(uri, 'utf8');
+  return yaml.load(envFile) as unknown;
+};
+
+export type Config = z.infer<typeof ConfigSchema>;
+
+export const CONFIG = (() => {
+  const file = loadFile('config/env.yaml');
+
+  const result = ConfigSchema.safeParse(file);
+  if (!result.success) {
+    console.error('Ошибка валидации конфигурации:', result.error.format());
+    throw new Error('Конфигурация не валидна');
+  }
+
+  return result.data;
+})();
