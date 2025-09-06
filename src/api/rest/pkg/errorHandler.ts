2ecdbfbe9e376cdff6f4ddc60e2b972a196b4a1f
sergeipechinov <spechinov@gmail.com>
2025-09-06 12:31:13 +0400

    Save

--- src/api/rest/pkg/errorHandler.ts
+++ src/api/rest/pkg/errorHandler.ts
@@ -3,6 +3,7 @@ import { hasZodFastifySchemaValidationErrors, isResponseSerializationError } fro
 
 export const errorHandler = (error: FastifyError, request: FastifyRequest, reply: FastifyReply) => {
   const params: Record<string, any> = {
+    statusCode: 500,
     originalUrl: request.originalUrl,
     timestamp: Date.now(),
   };
@@ -12,11 +13,9 @@ export const errorHandler = (error: FastifyError, request: FastifyRequest, reply
     params.message = error.message;
     params.isValidationError = true;
     request.log.error(error.message);
-  }
-
-  if (isResponseSerializationError(error)) {
-    params.statusCode = 500;
+  } else {
     request.log.error(error);
   }
+
   reply.code(params.statusCode).send(params);
 };
