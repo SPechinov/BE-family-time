cd35fbcc8c207dc52d3d18b0404262b14aba6cfe
sergeipechinov <spechinov@gmail.com>
2025-09-08 19:01:05 +0400

    Save

--- src/api/rest/routes/auth/index.ts
+++ src/api/rest/routes/auth/index.ts
@@ -14,7 +14,9 @@ export class AuthRoutesController {
 
   #register() {
     this.#fastify.post('/registration-begin', { schema: SCHEMA_REGISTRATION_BEGIN }, async (request, reply) => {
-      return { hello: 'world' };
+
+      console.log(request.body);
+      return null;
     });
   }
 }
