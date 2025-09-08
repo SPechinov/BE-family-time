9094fb92f4437bd06251ed578a2db92717e9cf93
sergeipechinov <spechinov@gmail.com>
2025-09-08 15:27:11 +0400

    Save

--- src/api/rest/routes/auth/schemas.ts
+++ src/api/rest/routes/auth/schemas.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 import { FastifySchema } from 'fastify';
-import { getDefaultResponse } from '../../pkg';
+import { getDefaultSchemaResponse } from '../../pkg';
 
 export const SCHEMA_REGISTRATION_BEGIN: FastifySchema = {
   body: z.object({
     email: z.email().nonempty().describe('Email address'),
   }),
   response: {
-    ...getDefaultResponse(),
+    ...getDefaultSchemaResponse(),
     200: z.null(),
   },
 };
