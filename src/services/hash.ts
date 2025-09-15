b4a0a9267ff89e12448e6ef3ab945e977fa55f09
sergeipechinov <spechinov@gmail.com>
2025-09-15 20:34:46 +0400

    Save

--- src/services/hash.ts
+++ src/services/hash.ts
@@ -1,8 +1,10 @@
 import { IHashService, IHashServiceConfig } from '@/domain/services';
 import crypto from 'crypto';
 
+const ALGORITHM = 'sha512';
+
 export class HashService implements IHashService {
   hash(value: string, config: IHashServiceConfig): string {
-    return crypto.createHmac('sha256', config.salt).update(value).digest('hex');
+    return crypto.createHmac(ALGORITHM, config.salt).update(value).digest('hex');
   }
 }
