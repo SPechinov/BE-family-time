9eac5a1ceb126324bcfda957ac6b4b77e8a5e385
Sergei Pechinov <spechinov@gmail.com>
2026-01-26 20:39:10 +0400

    Save

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,34 @@
-import { benchmarkHashPasswordService } from './tests/benchamarks';
-benchmarkHashPasswordService();
+import { CONFIG } from '@/config';
+import { newPostgresConnection, newRedisConnection } from '@/pkg';
+import { newApiRest } from '@/api/rest';
 
-// import { CONFIG } from '@/config';
-// import { newPostgresConnection, newRedisConnection } from '@/pkg';
-// import { newApiRest } from '@/api/rest';
+const run = async () => {
+  const [redis, postgres] = await Promise.all([
+    newRedisConnection(CONFIG.redis),
+    newPostgresConnection(CONFIG.postgres),
+  ]);
 
-// const run = async () => {
-//   const [redis, postgres] = await Promise.all([
-//     newRedisConnection(CONFIG.redis),
-//     newPostgresConnection(CONFIG.postgres),
-//   ]);
+  const apiRest = await newApiRest({
+    redis,
+    postgres,
+  });
 
-//   const apiRest = await newApiRest({
-//     redis,
-//     postgres,
-//   });
+  const destroyApp = async () => {
+    await apiRest.close();
+    console.log('\n');
+    console.log('API сервер выключен');
 
-//   const destroyApp = async () => {
-//     await apiRest.close();
-//     console.log('\n');
-//     console.log('API сервер выключен');
+    redis.destroy();
+    console.log('Redis отключен');
 
-//     redis.destroy();
-//     console.log('Redis отключен');
+    postgres.end();
+    console.log('PostgreSQL отключен');
 
-//     postgres.end();
-//     console.log('PostgreSQL отключен');
+    process.exit(0);
+  };
 
-//     process.exit(0);
-//   };
+  process.on('SIGINT', destroyApp);
+  process.on('SIGTERM', destroyApp);
+};
 
-//   process.on('SIGINT', destroyApp);
-//   process.on('SIGTERM', destroyApp);
-// };
-
-// run();
+run();
