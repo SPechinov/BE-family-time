5610123ce6548f40d0ec83353b20b42c77a2b881
Sergei Pechinov <spechinov@gmail.com>
2026-02-02 16:56:33 +0400

    Save

---
+++ src/api/rest/composites/auth.ts
@@ -0,0 +1,99 @@
+import { RedisClient } from '@/pkg';
+import { FastifyInstance } from 'fastify';
+import { Pool } from 'pg';
+import { AuthRoutesController } from '../routes/auth';
+import {
+  CryptoService,
+  HashPasswordService,
+  HashService,
+  OtpCodesService,
+  RateLimiterService,
+  UsersService,
+} from '@/services';
+import { CONFIG } from '@/config';
+import { UsersRepository } from '@/repositories/db';
+import { AuthUseCases } from '@/useCases';
+
+export class AuthComposite {
+  #fastifyInstance: FastifyInstance;
+  #redis: RedisClient;
+  #postgres: Pool;
+
+  constructor(props: { fastifyInstance: FastifyInstance; redis: RedisClient; postgres: Pool }) {
+    this.#fastifyInstance = props.fastifyInstance;
+    this.#postgres = props.postgres;
+    this.#redis = props.redis;
+
+    this.#register();
+  }
+
+  #register() {
+    const registrationOtpCodesService = new OtpCodesService({
+      redis: this.#redis,
+      prefix: 'auth-registration-otp',
+      codeLength: CONFIG.codesLength.registration,
+      ttlSec: CONFIG.ttls.registrationSec,
+    });
+
+    const registrationStartRateLimiterService = new RateLimiterService({
+      redis: this.#redis,
+      prefix: 'auth-registration-start-rate-limiter',
+      maxAttempts: 3,
+      windowSec: 600,
+    });
+
+    const registrationEndRateLimiterService = new RateLimiterService({
+      redis: this.#redis,
+      prefix: 'auth-registration-end-rate-limiter',
+      maxAttempts: 3,
+      windowSec: 600,
+    });
+
+    const forgotPasswordOtpCodesService = new OtpCodesService({
+      redis: this.#redis,
+      prefix: 'auth-forgot-password-otp',
+      codeLength: CONFIG.codesLength.forgotPassword,
+      ttlSec: CONFIG.ttls.forgotPasswordSec,
+    });
+
+    const forgotPasswordStartRateLimiterService = new RateLimiterService({
+      redis: this.#redis,
+      prefix: 'auth-forgot-password-rate-limiter',
+      maxAttempts: 3,
+      windowSec: 600,
+    });
+
+    const forgotPasswordEndRateLimiterService = new RateLimiterService({
+      redis: this.#redis,
+      prefix: 'auth-forgot-password-rate-limiter',
+      maxAttempts: 3,
+      windowSec: 600,
+    });
+
+    const userHashService = new HashService({
+      salt: CONFIG.salts.hashCredentials,
+    });
+
+    const hashPasswordService = new HashPasswordService();
+    const cryptoService = new CryptoService();
+
+    const usersRepository = new UsersRepository({ pool: this.#postgres });
+    const userService = new UsersService({
+      usersRepository,
+      hashService: userHashService,
+      hashPasswordService,
+      cryptoService,
+    });
+    const authUseCases = new AuthUseCases({
+      userService,
+      registrationOtpCodesService,
+      forgotPasswordOtpCodesService,
+      registrationStartRateLimiterService,
+      registrationEndRateLimiterService,
+      forgotPasswordStartRateLimiterService,
+      forgotPasswordEndRateLimiterService,
+    });
+
+    new AuthRoutesController({ fastify: this.#fastifyInstance, useCases: authUseCases }).register();
+  }
+}
--- src/api/rest/index.ts
+++ src/api/rest/index.ts
@@ -2,17 +2,7 @@ import { newFastify, RedisClient } from '@/pkg';
 import { CONFIG } from '@/config';
 import { Pool } from 'pg';
 import { globalErrorHandler } from './utils';
-import { AuthRoutesController } from './routes/auth';
-import { AuthUseCases } from '@/useCases/auth';
-import { UsersRepository } from '@/repositories/db';
-import {
-  CryptoService,
-  HashPasswordService,
-  HashService,
-  OtpCodesService,
-  RateLimiterService,
-  UsersService,
-} from '@/services';
+import { AuthComposite } from './composites';
 
 interface Props {
   redis: RedisClient;
@@ -24,75 +14,9 @@ export const newApiRest = async (props: Props) => {
     errorHandler: globalErrorHandler,
   });
 
-  const registrationOtpCodesService = new OtpCodesService({
-    redis: props.redis,
-    prefix: 'auth-registration-otp',
-    codeLength: CONFIG.codesLength.registration,
-    ttlSec: CONFIG.ttls.registrationSec,
-  });
-
-  const registrationStartRateLimiterService = new RateLimiterService({
-    redis: props.redis,
-    prefix: 'auth-registration-start-rate-limiter',
-    maxAttempts: 3,
-    windowSec: 600,
-  });
-
-  const registrationEndRateLimiterService = new RateLimiterService({
-    redis: props.redis,
-    prefix: 'auth-registration-end-rate-limiter',
-    maxAttempts: 3,
-    windowSec: 600,
-  });
-
-  const forgotPasswordOtpCodesService = new OtpCodesService({
-    redis: props.redis,
-    prefix: 'auth-forgot-password-otp',
-    codeLength: CONFIG.codesLength.forgotPassword,
-    ttlSec: CONFIG.ttls.forgotPasswordSec,
-  });
-
-  const forgotPasswordStartRateLimiterService = new RateLimiterService({
-    redis: props.redis,
-    prefix: 'auth-forgot-password-rate-limiter',
-    maxAttempts: 3,
-    windowSec: 600,
-  });
-
-  const forgotPasswordEndRateLimiterService = new RateLimiterService({
-    redis: props.redis,
-    prefix: 'auth-forgot-password-rate-limiter',
-    maxAttempts: 3,
-    windowSec: 600,
-  });
-
-  const userHashService = new HashService({
-    salt: CONFIG.salts.hashCredentials,
-  });
-
-  const hashPasswordService = new HashPasswordService();
-  const cryptoService = new CryptoService();
-
-  const usersRepository = new UsersRepository({ pool: props.postgres });
-  const userService = new UsersService({
-    usersRepository,
-    hashService: userHashService,
-    hashPasswordService,
-    cryptoService,
-  });
-  const authUseCases = new AuthUseCases({
-    userService,
-    registrationOtpCodesService,
-    forgotPasswordOtpCodesService,
-    registrationStartRateLimiterService,
-    registrationEndRateLimiterService,
-    forgotPasswordStartRateLimiterService,
-    forgotPasswordEndRateLimiterService,
-  });
-
   fastify.register(
     (instance) => {
-      new AuthRoutesController({ fastify: instance, useCases: authUseCases }).register();
+      new AuthComposite({ fastifyInstance: instance, redis: props.redis, postgres: props.postgres });
     },
     { prefix: '/api' },
   );
