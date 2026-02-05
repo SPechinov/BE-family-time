5dab12aa8f6c912f6828d1b4cce72f7c783c4d86
sergei <spechinov@gmail.com>
2026-02-05 04:38:06 -0800

    Save

--- src/api/rest/hooks/onRequest.ts
+++ src/api/rest/hooks/onRequest.ts
@@ -1,7 +1,14 @@
 import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
 
 export const onRequest = (request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => {
-  request.log.info({ url: request.url }, 'incoming request');
+  request.log.info(
+    {
+      method: request.method,
+      url: request.url,
+      userAgent: request.headers['user-agent'],
+    },
+    'incoming request',
+  );
   request.startRequestTimestamp = Date.now();
   done();
 };
