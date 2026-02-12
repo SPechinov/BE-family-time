53206c628f03f9a385333bdcd300a75a4313a12f
sergei <spechinov@gmail.com>
2026-02-12 19:47:52 +0400

    Save

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/benchamarks/'],
+  testPathIgnorePatterns: ['/node_modules/', '/benchmarks/'],
   collectCoverageFrom: ['src/**/*.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
