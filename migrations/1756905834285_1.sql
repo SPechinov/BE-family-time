e0290f0bcef440d1582acefd1d213a95a8cf0827
sergei <spechinov@gmail.com>
2026-02-17 17:59:54 +0400

    Save

--- migrations/1756905834285_1.sql
+++ migrations/1756905834285_1.sql
@@ -1,8 +1,6 @@
 -- Up Migration
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     encryption_salt UUID NOT NULL DEFAULT gen_random_uuid(),
     email_encrypted BYTEA,
     email_hashed BYTEA,
@@ -19,7 +17,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS uid_users_id ON users (id);
 CREATE UNIQUE INDEX IF NOT EXISTS uid_users_email_hashed ON users (email_hashed) WHERE email_hashed IS NOT NULL;
 CREATE UNIQUE INDEX IF NOT EXISTS uid_users_phone_hashed ON users (phone_hashed) WHERE phone_hashed IS NOT NULL;
 
-CREATE OR REPLACE FUNCTION update_updated_at_column()
+CREATE OR REPLACE FUNCTION update_updated_at_users()
 RETURNS TRIGGER AS $$
 BEGIN
   NEW.updated_at = NOW();
@@ -32,11 +30,11 @@ DROP TRIGGER IF EXISTS update_users_updated_at ON users;
 CREATE TRIGGER update_users_updated_at
     BEFORE UPDATE ON users
     FOR EACH ROW
-    EXECUTE FUNCTION update_updated_at_column();
+    EXECUTE FUNCTION update_updated_at_users();
 
 -- Down Migration
 DROP TRIGGER IF EXISTS update_users_updated_at ON users;
-DROP FUNCTION IF EXISTS update_updated_at_column();
+DROP FUNCTION IF EXISTS update_updated_at_users();
 DROP INDEX IF EXISTS uid_users_phone_hashed;
 DROP INDEX IF EXISTS uid_users_email_hashed;
 DROP INDEX IF EXISTS uid_users_id;
