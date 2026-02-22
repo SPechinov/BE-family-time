90ff5349a0a3d4737a7876406f19e4cdd3bfdc41
sergei <spechinov@gmail.com>
2026-02-22 12:39:45 +0400

    Save

--- src/api/rest/routes/groups/constants.ts
+++ src/api/rest/routes/groups/constants.ts
@@ -3,9 +3,9 @@ export const PREFIX = '/groups';
 export const ROUTES = Object.freeze({
   getList: '/',
   create: '/',
-  get: '/:id',
-  patch: '/:id',
-  delete: '/:id',
-  inviteUser: '/:id/inviteUser',
-  excludeUser: '/:id/excludeUser',
+  get: '/:groupId',
+  patch: '/:groupId',
+  delete: '/:groupId',
+  inviteUser: '/:groupId/inviteUser',
+  excludeUser: '/:groupId/excludeUser',
 });
