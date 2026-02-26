9929885e7179e5286ea412df34407f2b0f832878
sergei <spechinov@gmail.com>
2026-02-26 12:07:31 +0400

    Save

--- migrations/4_create_group_calendar_table.sql
+++ migrations/4_create_group_calendar_table.sql
@@ -2,16 +2,17 @@
 
 CREATE TABLE IF NOT EXISTS group_calendar
 (
-    id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    group_id        UUID         NOT NULL REFERENCES groups (id) ON DELETE CASCADE,
-    creator_user_id UUID         NOT NULL REFERENCES users (id) ON DELETE SET NULL,
-    title           VARCHAR(100) NOT NULL,
-    description     VARCHAR(1000),
-    event_type      VARCHAR(20),
-    iteration_type  VARCHAR(20)  NOT NULL,
-    start_date      TIMESTAMP    NOT NULL,
-    end_date        TIMESTAMP        DEFAULT NULL,
-    created_at      TIMESTAMP        DEFAULT NOW(),
+    id                 UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    group_id           UUID         NOT NULL REFERENCES groups (id) ON DELETE CASCADE,
+    creator_user_id    UUID         NOT NULL REFERENCES users (id) ON DELETE SET NULL,
+    title              VARCHAR(100) NOT NULL,
+    description        VARCHAR(1000),
+    event_type         VARCHAR(20),
+    iteration_type     VARCHAR(20)  NOT NULL,
+    recurrence_pattern JSONB,
+    start_date         TIMESTAMP    NOT NULL,
+    end_date           TIMESTAMP        DEFAULT NULL,
+    created_at         TIMESTAMP        DEFAULT NOW(),
 
     CONSTRAINT check_event_type CHECK (
         event_type IN ('birthday', 'vacation', 'holiday')
@@ -19,6 +20,11 @@ CREATE TABLE IF NOT EXISTS group_calendar
 
     CONSTRAINT check_iteration_type CHECK (
         iteration_type IN ('oneTime', 'weekly', 'monthly', 'yearly')
+        ),
+
+    CONSTRAINT check_recurrence_pattern CHECK (
+        (event_type IN ('oneTime', 'yearly') AND recurrence_pattern IS NULL) OR
+        (event_type IN ('weekly', 'monthly') AND recurrence_pattern IS NOT NULL)
         ),
 
     CONSTRAINT check_dates CHECK (end_date >= start_date)
