21fc2e3972dac8e49c4e6078130c89dc5acb298e
sergei <spechinov@gmail.com>
2026-02-27 22:37:54 +0400

    Save

--- src/api/rest/schemas/utils.ts
+++ src/api/rest/schemas/utils.ts
@@ -1,3 +1,20 @@
-export const isXss = (val: string) => {
-  return !/<[^>]*>/.test(val);
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+export const isXss = (val: string): boolean => {
+  const dangerousUriPatterns = [/javascript\s*:/i, /vbscript\s*:/i, /data\s*:/i];
+
+  if (dangerousUriPatterns.some((pattern) => pattern.test(val))) {
+    return false;
+  }
+
+  const sanitized = DOMPurify.sanitize(val, {
+    ALLOWED_TAGS: [],
+    ALLOWED_ATTR: [],
+  });
+
+  return val === sanitized;
 };
