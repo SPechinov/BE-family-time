dabda8ed2e6ae5d365d255118f63e57f35ba23d2
sergei <spechinov@gmail.com>
2026-04-14 14:29:51 +0400

    Savw

--- src/api/rest/constants/index.ts
+++ src/api/rest/constants/index.ts
@@ -8,7 +8,7 @@ export const HEADER_NAME = Object.freeze({
 export const ACCESS_TOKEN_COOKIE_CONFIG: CookieSerializeOptions = Object.freeze({
   httpOnly: true,
   secure: isProd(),
-  sameSite: isProd(),
+  sameSite: isProd() ? 'strict' : false,
   maxAge: CONFIG.jwt.access.expiry / 1000,
   path: '/',
 });
@@ -16,7 +16,7 @@ export const ACCESS_TOKEN_COOKIE_CONFIG: CookieSerializeOptions = Object.freeze(
 export const REFRESH_TOKEN_COOKIE_CONFIG: CookieSerializeOptions = Object.freeze({
   httpOnly: true,
   secure: isProd(),
-  sameSite: isProd(),
+  sameSite: isProd() ? 'strict' : false,
   maxAge: CONFIG.jwt.refresh.expiry / 1000,
   path: '/',
 });
