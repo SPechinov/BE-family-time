7a9bacc9843c30942ffbc1abf1b017b1c36873ec
sergei <spechinov@gmail.com>
2026-04-14 17:20:34 +0400

    Save

--- src/domains/repositories/stores/index.ts
+++ src/domains/repositories/stores/index.ts
@@ -1,2 +1,1 @@
-export * from './refreshTokens';
 export * from './otpCodes';
--- src/domains/repositories/stores/refreshTokens.ts
+++
@@ -1,9 +0,0 @@
-import { UserId } from '@/entities';
-
-export interface IRefreshTokensStore {
-  save(props: { userId: UserId; refreshToken: string; expiresAt: Date }): Promise<void>;
-  hasInStore(props: { userId: UserId; refreshToken: string }): Promise<boolean>;
-  delete(props: { userId: UserId; refreshToken: string }): Promise<void>;
-  deleteAll(props: { userId: UserId }): Promise<void>;
-  getAllByUserId(props: { userId: UserId }): Promise<string[]>;
-}
